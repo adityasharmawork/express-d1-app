9cc65660668f3bd08048dc33672ec3a1dcc20080
Aditya Sharma <adityasharma2005123@gmail.com>
2025-12-07 13:49:18 +0530

    Initialized a basic Express.js Server on Cloudflare Workers

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,38 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+// /**
+//  * Welcome to Cloudflare Workers! This is your first worker.
+//  *
+//  * - Run `npm run dev` in your terminal to start a development server
+//  * - Open a browser tab at http://localhost:8787/ to see your worker in action
+//  * - Run `npm run deploy` to publish your worker
+//  *
+//  * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
+//  * `Env` object can be regenerated with `npm run cf-typegen`.
+//  *
+//  * Learn more at https://developers.cloudflare.com/workers/
+//  */
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+// export default {
+// 	async fetch(request, env, ctx): Promise<Response> {
+// 		return new Response('Hello World!');
+// 	},
+// } satisfies ExportedHandler<Env>;
+
+
+
+
+
+import { env } from "cloudflare:workers";
+import { httpServerHandler } from  "cloudflare:node";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.json({message: "Express.js running on Cloudflare Workers!"});
+});
+
+app.listen(3000);
+
+export default httpServerHandler({ port : 3000 });
